4ffcd90e9adb76dba84e8537804bed9e6790a48f
ThanThoai <huyen.btt192458@sis.hust.edu.vn>
2021-01-12 17:35:37 +0700

    add BERT encoding

--- ratsql/resources/pretrained_embeddings.py
+++ ratsql/resources/pretrained_embeddings.py
@@ -2,17 +2,17 @@ import abc
 import functools
 import os
 import time
-
+from typing import Dict
 import bpemb
 import corenlp
 import torch
 import torchtext
-
+import datetime
 from ratsql.resources import corenlp
 from ratsql.utils import registry
 from fairseq.data.encoders.fastbpe import fastBPE
 from fairseq.models.roberta import RobertaModel
-
+from vncorenlp import VNCoreNLP
 
 class Embedder(metaclass=abc.ABCMeta):
 
@@ -116,25 +116,83 @@ class BPEmb(Embedder):
         self.vectors = self.vectors.to(device)
 
 
-# class BPE():
-#     bpe_codes = "PhoBERT_base_fairseq/bpe.codes"
+class Tokenizer:
 
-# args = BPE()
+    def __init__(self, path = './vncorenlp/VnCoreNLP-1.1.1.jar'):
 
-# @registry.register("word_emb", "vi_bpe")
-# class BPEVi(Embedder):
-#     def __init__(self, dim, vocab_size, lang='vi'):
-#         self.BertVi = RobertaModel.from_pretrained('PhoBERT_base_fairseq', checkpoint_file='model.pt')
-#         self.BertVi.bpe = fastBPE(args)
-#         self.dim = dim
+        self.path = path 
+        self.rdrsegenter = VnCoreNLP(self.path, annotators="wseg", max_heap_size='-Xmx500m')
 
-#         self.vectors = torch.from_numpy(self.BertVi.bpe.vectors)
+    def tokenizer(self, sentences, return_string = False):
+        
+        re_sentences = self.rdrsegenter.tokenize(sentences)
+        if return_string:
+            return " ".join([s for s in re_sentences])
+        return re_sentences
+
+
+
+class BPE_BASE():
+    bpe_codes = "./PhoBERT_base_fairseq/bpe.codes"
+
+class BPE_LARGE():
+    bpe_codes = "./PhoBERT_large_fairseq/bpe.codes"
+
+
+@registry.register("word_emb", "phoBert")
+class phoBert(Embedder):
     
-#     def tokenize(self, text):
-#         return self.BertVi.encode(text)
-    
-#     def untokenize(self, tokens):
-#         return self.BertVi.decode(tokens)
+    def __init__(self, BERT_MODEL, write_file = True):
+        
+        self.method = BERT_MODEL
+        self.dict_model = {
+            "BERT_BASE" : {
+                "NAME" : "./PhoBERT_base_fairseq",
+                "PATH_CHECKPOINT_FILE" : "model.pt"
+            },
 
-#     def lookup(self, token):
+            "BERT_LARGE" : {
+                "NAME" : "./PhoBERT_large_fairseq",
+                "PATH_CHECKPOINT_FILE" : "model.pt"
+            }
 
+        }
+        assert self.method in self.dict_model.keys(), "[ERROR] Method {} not supported!!!!".format(self.method)
+        self.pho_bert = RobertaModel(self.dict_model[self.method]["NAME"], self.dict_model[self.method]["PATH_CHECKPOINT_FILE"])
+        self.tokenize = Tokenizer()
+        self.pho_bert.eval()
+        if self.method == "BERT_BASE":
+            args = BPE_BASE()
+        else:
+            args = BPE_LARGE()
+        self.pho_bert.bpe = fastBPE(args)
+        self.embeding = {}
+        self.write_file = write_file
+
+    def run(self, data : List[Dict]) -> None:
+        print(datetime.now().strftime("%H:%M:%S"), f"\t INFO: Extract Features using BERT: {self.method}")
+        for d in tqdm.tqdm(data):
+            key = d['question_id']
+            assert key not in self.embeding.keys(), "ERROR"
+            question = self.tokenize.tokenizer(d["question"], return_string = True)
+            token_idx = self.pho_bert.encode(question)
+            doc = self.pho_bert.extract_features_aligned_to_words(question)
+            self.embeding[key] = {
+                "vector" : {},
+                "idx_token" : token_idx[1:-1]
+            }
+            for tok in doc[1: -1]:
+                self.embeding[key]["vector"][str(tok)] = tok.vector
+        
+        if self.write_file:
+            path = "./dataset/text/extract_bert"
+            print(datetime.now().strftime("%H:%M:%S"), f"\t INFO: Saving extract features to file")
+            torch.save(self.embeding, os.path.join(path, self.method + '.pt'))
+
+
+    def extract(self, question: str) -> Dict:
+        result = {}
+        doc = self.pho_bert.extract_features_aligned_to_words(self.tokenize.tokenizer(question))
+        for tok in doc[1: len(doc) - 1]:
+            result[tok] = tok.vector
+        return result
