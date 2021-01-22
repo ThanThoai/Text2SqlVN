c7f40ddafa22be02f9907b1f13a1834bb4db541a
Bùi Xuân Thoại (VNCDLL-CTÐTKSAI) <huyen.btt192458@sis.hust.edu.vn>
2021-01-22 15:29:57 +0700

    update src

--- scripts/split_spider_by_db.py
+++ scripts/split_spider_by_db.py
@@ -4,7 +4,7 @@ import argparse
 
 from collections import defaultdict
 from typing import List, Dict
-
+import tqdm
 
 def main(spider_path, duorat_path) -> None:
 
@@ -24,10 +24,10 @@ def main(spider_path, duorat_path) -> None:
         assert db_id not in grouped_payload
         grouped_payload[db_id] = item
 
-    for db_id, item in grouped_payload.items():
+    for db_id, item in tqdm.tqdm(grouped_payload.items()):
         mkdir_p(os.path.join(duorat_path, db_id))
         with open(os.path.join(duorat_path, db_id, "tables.json"), "wt") as write_fp:
-            json.dump([item], write_fp, indent=2)
+            json.dump([item], write_fp, indent=2, ensure_ascii= False)
 
     for examples_path in examples_paths:
         with open(os.path.join(spider_path, examples_path), "r") as read_fp:
@@ -38,10 +38,10 @@ def main(spider_path, duorat_path) -> None:
             db_id: str = item['db_id']
             grouped_payload[db_id].append(item)
 
-        for db_id, payload_group in grouped_payload.items():
+        for db_id, payload_group in tqdm.tqdm(grouped_payload.items()):
             mkdir_p(os.path.join(duorat_path, db_id))
             with open(os.path.join(duorat_path, db_id, "examples.json"), "wt") as write_fp:
-                json.dump(payload_group, write_fp, indent=2)
+                json.dump(payload_group, write_fp, indent=2, ensure_ascii=False)
 
 
 if __name__ == '__main__':
